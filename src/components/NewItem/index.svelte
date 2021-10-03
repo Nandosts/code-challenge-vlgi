ccdddefb28db90aee8304d6a6a3988cbcc054a4b
nandosts <fernando.jsts@gmail.com>
2021-10-03 18:02:31 -0300

    Função de adicionar itens

---
+++ src/components/NewItem/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import "./styles.scss";
+
+  const dispatch = createEventDispatcher();
+
+  let value = "";
+
+  function handleKeyUp({ key }) {
+    if (key !== "Enter" || value.length === 0) return;
+    dispatch("newitem", value);
+    value = "";
+  }
+</script>
+
+<input
+  type="text"
+  on:keyup={handleKeyUp}
+  bind:value
+  placeholder="+ Item da lista"
+/>
--- src/components/Todo/index.svelte
+++ src/components/Todo/index.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { v4 as uuidv4 } from "uuid";
+
   import { tasks } from "../../stores.js";
 
   import TodoApi from "../../TodoApi.js";
 
   import "./styles.scss";
   import TodoItem from "../TodoItem/index.svelte";
+  import NewItem from "../NewItem/index.svelte";
+
+  function handleNewItem(e) {
+    $tasks = [
+      {
+        id: uuidv4(),
+        text: e.detail,
+        checked: false,
+      },
+      ...$tasks,
+    ];
+
+    TodoApi.save($tasks);
+  }
 
   function handleUpdate(e) {
     const index = $tasks.findIndex((item) => item.id === e.detail.id);
     $tasks[index] = e.detail;
-    TodoApi.save($tasks)
+    TodoApi.save($tasks);
   }
 
   function handleDelete(e) {
@@ -24,6 +40,7 @@
 </script>
 
 <div class="todo-list">
+  <NewItem on:newitem={handleNewItem} />
   {#each $tasks as item (item)}
     <TodoItem {...item} on:update={handleUpdate} on:delete={handleDelete} />
   {/each}
