decab3a30a0d8a0321d719a447c53900f4fd7515
nandosts <fernando.jsts@gmail.com>
2021-10-03 15:10:58 -0300

    handleUpdate e handleDelete

--- src/components/Todo/index.svelte
+++ src/components/Todo/index.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { tasks } from "../../stores.js";
-  
-  import TodoApi from "../../TodoApi.js"
-  
+
+  import TodoApi from "../../TodoApi.js";
+
   import "./styles.scss";
   import TodoItem from "../TodoItem/index.svelte";
+
+  function handleUpdate(e) {
+    const index = $tasks.findIndex((item) => item.id === e.detail.id);
+    $tasks[index] = e.detail;
+    TodoApi.save($tasks)
+  }
+
+  function handleDelete(e) {
+    $tasks = $tasks.filter((item) => item.id !== e.detail);
+    TodoApi.save($tasks);
+  }
 
   onMount(async () => {
     $tasks = await TodoApi.getAll();
@@ -14,6 +25,6 @@
 
 <div class="todo-list">
   {#each $tasks as item (item)}
-    <TodoItem {...item} />
+    <TodoItem {...item} on:update={handleUpdate} on:delete={handleDelete} />
   {/each}
 </div>
--- src/components/TodoItem/index.svelte
+++ src/components/TodoItem/index.svelte
@@ -1,15 +1,36 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import "./styles.scss";
-  
+
   export let id, text, checked;
+
+  const dispatch = createEventDispatcher();
+
+  function triggerUpdate() {
+    dispatch("update", { id, text, checked });
+  }
+
+  function handleClick() {
+    dispatch("delete", id);
+  }
 </script>
 
 <div class="todo-item" class:checked>
-  <input 
-  type="text" 
-  class="text-input" 
-  bind-value={text} 
-  readonly={checked} />
+  <input
+    type="checkbox"
+    class="checked-box"
+    bind:checked
+    on:change={() => triggerUpdate()}
+  />
 
-  <input type="checkbox" class="checked-box" bind:checked={checked} />
+  <input
+    type="text"
+    class="text-input"
+    bind-value={text}
+    readonly={checked}
+    on:keyup={({ key, target }) => key === "Enter" && target.blur()}
+    on:blur={() => triggerUpdate()}
+  />
+
+  <ion-icon name="close" class="delete-icon" on:click={handleClick} />
 </div>
